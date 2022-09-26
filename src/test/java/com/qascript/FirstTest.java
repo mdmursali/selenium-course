60ed2f9f8789cbfb48abc7e6dbeee02a2444a71d
mdmursali <112893169+mdmursali@users.noreply.github.com>
2022-09-26 11:20:27 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,7 +1,5 @@
 package com.qascript;
 
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
